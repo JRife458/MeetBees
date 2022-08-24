9a291722fb4b9098a7c191a8d688d92547cb2253
Justin <mancussion@gmail.com>
2022-08-24 16:39:24 -0400

    added dev route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,4 +7,16 @@ router.get('/hello/world', function(req, res) {
   res.send('Hello World!');
 });
 
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+// ...
+
 module.exports = router;
